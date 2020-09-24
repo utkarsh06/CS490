b14af845f382639f05f7017b027ac4c05e8081b4
Josue Solorzano <jrs487@scarletmail.rutgers.edu>
2020-09-24 16:19:11 -0400

    Front end

--- includes/login.php
+++ includes/login.php
@@ -1,5 +1,26 @@
 <?php
+ if(isset($_POST['login']) && !empty($_POST['login'])){
+	$email = $_POST['email'];
+	$password = $_POST['password'];
 
+	if(!empty($email) or !empty($password)){
+		$email = $getFromU->checkInput($email);
+		$password = $getFromU->checkInput($password);
+
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+			$error = "Invalid format";
+		}else{
+			if($getFromU-> login($email, $password) === false){
+				$error = "The email or password is incorrect!";
+
+			}
+		}
+
+
+	}else{
+		$error = "Please enter username and password!";
+	}
+}
 ?>
 <div class="login-div">
 <form method="post"> 
@@ -13,11 +34,14 @@
 		<li>
 		  <input type="checkbox" Value="Remember me">Remember me
 		</li>
+	
+		<?php
+		  if(isset($error)){
+		     echo ' <li class="error-li">
+	 			   <div class="span-fp-error">'.$error.'</div>
+			     </li> ';
+	 	  }
+		?>
 	</ul>
-	<!--
-	 <li class="error-li">
-	  <div class="span-fp-error"></div>
-	 </li> 
-	-->
 	</form>
 </div>
--- includes/signup-form.php
+++ includes/signup-form.php
@@ -1,5 +1,32 @@
 <?php 
+ if(isset($_POST['signup'])){
+	$screenName = $_POST['screenName'];
+	$password   = $_POST['password'];
+	$email      = $_POST['email'];
+	$error      = '';
 
+	if(empty($screenName) or empty($password) or empty($email)){
+		$error = 'All field are required';
+	}else{
+		$email      = $getFromU->checkInput($email);
+		$screenName = $getFromU->checkInput($screenName);
+		$password   = $getFromU->checkInput($password);
+		
+		if(!filter_var($email)){
+			$error = 'Invalid email format';
+		}else if(strlen($screenName) > 20){
+			$error = 'Name must be in between 6-20 characters';
+		}else if (strlen($password) < 5){
+			$error = 'Password is too short';
+		}else {
+			if($getFromU->checkEmail($email) ===true){
+				$error = 'Email is already in use';
+			}else{
+
+			}
+		}
+	}
+}
 ?>
 <form method="post">
 <div class="signup-div"> 
@@ -18,10 +45,13 @@
 			<input type="submit" name="signup" Value="Signup for Twitter">
 		</li>
 	</ul>
-	<!--
-	 <li class="error-li">
-	  <div class="span-fp-error"></div>
-	 </li> 
-	-->
+	<?php
+		if(isset($error)){
+			echo ' <li class="error-li">
+	  <div class="span-fp-error">'.$error.'</div>
+	 </li> ';
+		}
+	?>
+	
 </div>
 </form>
