c6d4e44192e9b08ad99df1d4cb2e809b4c004f60
utkarsh06 <utkarsh.gaur06@gmail.com>
2020-10-08 20:02:46 -0400

    being stupid

--- core/database/connection.php
+++ core/database/connection.php
@@ -1,15 +1,12 @@
 <?php
-   $servername = 'us-cdbr-east-02.cleardb.com'
-   $database = 'heroku_0204a9e86e1cf68' 
-   $username = 'b253828cee1fe7'
-   $password = '86875cb0'
 
-   $dsn = 'mysql:host=$servername; dbname=$database;'; ;
+
+   $dsn = 'mysql:host=localhost; dbname=tweety' ;
    $user = 'root';
    $pass = '';
 
    try{
-   	   $pdo = new PDO($dsn, $username, $password);
+   	   $pdo = new PDO($dsn, $user, $pass);
    } catch(PDOException $e){
    		echo 'Connection error! ' . $e->getMessage();
    }
