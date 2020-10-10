0a411737f963e3c517db1ddca94c5723aad2eb80
Josue Solorzano <jrs487@scarletmail.rutgers.edu>
2020-10-10 14:30:24 -0400

    search.js update again

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,7 +3,7 @@ $(function(){
  	var search = $(this).val();
  	//$.post('http://localhost/twitterclone/core/ajax/search.php', {search:search}, function(data){
  		//$('.search-result').html(data);
- 		$.post('https://calm-ocean-67152.herokuapp.com/core/ajax/search.php', {search:search}, function(data){
+ 	$.post('https://calm-ocean-67152.herokuapp.com', {search:search}, function(data){
  		$('.search-result').html(data);
  	});
  });
