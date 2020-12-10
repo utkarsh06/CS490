0f0d0c814fc4b309e0b82ee6d785c46b2d0a8093
Josue Solorzano <jrs487@scarletmail.rutgers.edu>
2020-12-10 16:06:18 -0500

    new change

--- assets/js/snakeGame.js
+++ assets/js/snakeGame.js
@@ -4,49 +4,49 @@ window.onload=function() {
     document.addEventListener("keydown",keyPush);
     setInterval(game,1000/15);
 }
-px=py=10;
+positionX=positionY=10;
 gs=tc=20;
 ax=ay=15;
 xv=yv=0;
 trail=[];
 tail = 5;
 function game() {
-    px+=xv;
-    py+=yv;
-    if(px<0) {
-        px= tc-1;
+    positionX+=xv;
+    positionY+=yv;
+    if(positionX<0) {
+        positionX= tc-1;
     }
-    if(px>tc-1) {
-        px= 0;
+    if(positionX>tc-1) {
+        positionX= 0;
     }
-    if(py<0) {
-        py= tc-1;
+    if(positionY<0) {
+        positionY= tc-1;
     }
-    if(py>tc-1) {
-        py= 0;
+    if(positionY>tc-1) {
+        positionY= 0;
     }
     ctx.fillStyle="black";
     ctx.fillRect(0,0,canv.width,canv.height);
  
     ctx.fillStyle="lime";
     for(var i=0;i<trail.length;i++) {
-        ctx.fillRect(trail[i].xgs,trail[i].ygs,gs-2,gs-2);
-        if(trail[i].x==px && trail[i].y==py) {
+        ctx.fillRect(trail[i].x*gs,trail[i].y*gs,gs-2,gs-2);
+        if(trail[i].x==positionX && trail[i].y==positionY) {
             tail = 5;
         }
     }
-    trail.push({x:px,y:py});
+    trail.push({x:positionX,y:positionY});
     while(trail.length>tail) {
     trail.shift();
     }
  
-    if(ax==px && ay==py) {
+    if(ax==positionX && ay==positionY) {
         tail++;
-        ax=Math.floor(Math.random()tc);
-        ay=Math.floor(Math.random()tc);
+        ax=Math.floor(Math.random()*tc);
+        ay=Math.floor(Math.random()*tc);
     }
     ctx.fillStyle="red";
-    ctx.fillRect(axgs,aygs,gs-2,gs-2);
+    ctx.fillRect(ax*gs,ay*gs,gs-2,gs-2);
 }
 function keyPush(evt) {
     switch(evt.keyCode) {
